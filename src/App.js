822f02c3a34d295d1b38c12487d118ff52de5c0b
lokkib <lokkichebrikov@yandex.ru>
2022-12-17 16:27:57 +0300

    main components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+import pizzas from './assets/pizza.json';
+
+
+
 
 function App() {
+
+  fetch('https://639ca6d642e3ad69273867e2.mockapi.io/items').then(res => {
+    console.log(res)
+    return res.json()
+  }).then(array => console.log(array))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+         {pizzas.map((el) => <PizzaBlock key={el.id} {...el} />)}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+
+function Categories () {
+
+const [activeCategory, setActiveCategory] = useState(0)
+
+const categories = ['Все', 'Мясные', 'Вегетарианская','Гриль','Острые', 'Закрытые']
+
+const clickedCategory = (index) => {
+  setActiveCategory(index)
+}
+
+    return (
+      <div className="categories">
+                  <ul>
+                   {categories.map((el, index) => <li key={index} onClick={() => clickedCategory(index)} className={activeCategory === index? 'active' : ''}>{el}</li> )}
+                  </ul>
+                </div>
+    )
+  }
+
+  export default Categories
